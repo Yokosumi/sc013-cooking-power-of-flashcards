5442e84b23d646b8828fa8c60d352eb99f083aee
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-08-10 13:44:21 +0200

    refactor: Update Card component styling for better display of flashcards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ type CardProps = {
 export const Card = ({ display }: CardProps) => {
     return (
         <>
-            <section className="border-4 border-black text-green-500 font-bold text-2xl text-center  w-32 h-16 py-2 m-4 bg-gray-800 pointer">
+            <section className="flex justify-center items-center border-4 border-black text-green-500 font-bold text-2xl text-center p-4 w-64 h-32 m-4 bg-gray-800 pointer">
                 {display}
             </section>
         </>
