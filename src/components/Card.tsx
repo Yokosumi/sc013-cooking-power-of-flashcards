c7b778667aa0e00d044e6ed0ac6870af780db599
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-08-10 13:15:26 +0200

    first card ui done

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+type CardProps = {
+    display: string
+}
+
+export const Card = ({ display }: CardProps) => {
+    return (
+        <>
+            <section className="border-4 border-black text-green-500 font-bold text-2xl text-center  w-32 h-16 py-2 m-4 bg-gray-800">
+                {display}
+            </section>
+        </>
+    )
+}
